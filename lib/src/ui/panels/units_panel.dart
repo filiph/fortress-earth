b2120855b20d64afa404edad45ff4529486d7cbc
Filip Hracek <filip.hracek@gmail.com>
2019-01-26 22:09:31 -0800

    Show some info for units

--- lib/src/ui/panels/units_panel.dart
+++ lib/src/ui/panels/units_panel.dart
@@ -1,4 +1,5 @@
 import 'package:fortress_earth/src/ui/panels/panel.dart';
+import 'package:fortress_earth/src/ui/theme.dart';
 import 'package:fortress_earth/src/units.dart';
 import 'package:malison/malison.dart';
 
@@ -11,12 +12,15 @@ class UnitPanel extends Panel {
   @override
   void renderPanel(Terminal terminal) {
     terminal.writeAt(0, 0, "Units");
-    
+
     var y = 2;
     for (final key in units.units.keys) {
       final unit = units.units[key];
       terminal.drawChar(0, y, key, unit.color);
       terminal.writeAt(2, y, unit.name);
+
+      terminal.writeAt(
+          14, y, unit.hasArrived ? "STNDBY" : "TRANST", TextTheme.detail  );
 
       y++;
     }
--- lib/src/ui/theme.dart
+++ lib/src/ui/theme.dart
@@ -2,6 +2,7 @@ import 'package:malison/malison.dart';
 
 class TextTheme {
   static const normal = Color.lightGray;
+  static const detail = Color.darkGray;
   static const important = Color.white;
   static const ok = Color.darkGreen;
 }
